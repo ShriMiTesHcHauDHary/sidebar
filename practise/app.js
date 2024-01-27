7a47b8e3efd0edce141101b14cf864c1f6e0e9c4
mitesh <ShriMiTesHcHauDHary@gmail.com>
2024-01-27 14:11:36 +0530

    27-01-24

---
+++ practise/app.js
@@ -0,0 +1,12 @@
+const sidetogglebtn = document.querySelector(".sidebar-toggle")
+const closebtn = document.querySelector(".close-btn")
+const sidebar = document.querySelector(".sidebar")
+
+
+sidetogglebtn.addEventListener("click", function(){
+    sidebar.classList.toggle("show-sidebar")
+})
+
+closebtn.addEventListener("click", function(){
+    sidebar.classList.remove("show-sidebar")
+})
